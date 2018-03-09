edf713556eed5eb7ed0069807daee2340978a848
Hoezer <36734069+Hoezer@users.noreply.github.com>
2018-03-09 14:49:59 +0100

    Nieuwe environment

--- snaptogrid/Assets/CubePlacer.cs
+++ snaptogrid/Assets/CubePlacer.cs
@@ -3,31 +3,77 @@
 public class CubePlacer : MonoBehaviour
 {
     private Grid grid;
+    private GameObject Cube;
+    public Material Opaque;
+    public Material standaard; 
+
 
     private void Awake()
     {
         grid = FindObjectOfType<Grid>();
+        GetNewCube();
     }
 
     private void Update()
     {
+        MoveCubeByMouse();
+
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hitInfo;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hitInfo))
-            {
-                PlaceCubeNear(hitInfo.point);
-            }
+            PlaceANewCube();
         }
+        
+    }
+
+    private void PlaceANewCube()
+    {
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            PlaceCubeNear(hitInfo.point);
+            GetNewCube();
+            MoveCubeByMouse();
+        }
+
     }
 
     private void PlaceCubeNear(Vector3 clickPoint)
     {
         var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
-        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
-
+        Cube.transform.position = finalPosition;
+        Cube.GetComponent<Renderer>().material = standaard;
         //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+
+    private void MoveCubeByMouse()
+    {
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            MoveCube(hitInfo.point);
+        }
+
+    }
+
+    private void MoveCube(Vector3 clickPoint)
+    {
+        var newPosition = grid.GetNearestPointOnGrid(clickPoint);
+        Cube.transform.position = newPosition;
+    }
+
+    private void GetNewCube()
+    {
+        Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+       
+
+        /*Renderer rend = Cube.GetComponent<Renderer>();
+        rend.material =*/
+
+        Cube.GetComponent<Renderer>().material = Opaque;
+
     }
 }
--- snaptogrid/Assets/Grid.cs
+++ snaptogrid/Assets/Grid.cs
@@ -20,6 +20,8 @@ public class Grid : MonoBehaviour
 
         result += transform.position;
 
+        
+
         return result;
     }
 
