4b245f72389499158b3077623c64a59c0966a794
Hoezer <36734069+Hoezer@users.noreply.github.com>
2018-03-09 17:24:29 +0100

    j

---
+++ snaptogrid/Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+    public Button ButtonHouse;
+    // Use this for initialization
+    void Start () {
+        Button btnButtonHouse = ButtonHouse.GetComponent<Button>();
+        btnButtonHouse.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+}
